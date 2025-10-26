945c0dd87e55d13984b3cc7549e682e8ec653ca2
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-10-26 21:59:02 +0100

    used axios to fetch API data and displayed produtcs on the 'Products' page using bootstrap cards

--- src/pages/Prodottipage.jsx
+++ src/pages/Prodottipage.jsx
@@ -1,14 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import AppNav from "../components/AppNav";
 import AppFooter from "../components/AppFooter";
 export default function Prodottipage() {
+    const [products, setProducts] = useState([]);
+    function fetchProducts() {
+        console.log("Chiamo l'API...");
+        axios.get("https://fakestoreapi.com/products")
+            .then(res => {
+                console.log(res.data);
+                setProducts(res.data);
+            })
+            .catch(err => {
+                console.error("Errore:", err.message);
+            });
+    }
+    useEffect(fetchProducts, []);
     return (
         <>
             <header>
 
                 <AppNav />
             </header>
-            <main>
-                I miei prodotti
+            <main className="container py-4">
+                <h1>Prodotti</h1>
+                <div className="row g-4">
+                    {products.map((product) => (
+                        <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={product.id}>
+                            <div className="card h-100 text-center">
+                                <img src={product.image} className="card-img-top p-3" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{product.title}</h5>
+                                    <p className="card-text fw-bold">{product.price} €</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </main>
             <AppFooter />
         </>
